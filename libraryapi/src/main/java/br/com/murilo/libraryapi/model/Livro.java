a7c2577e09f071c18679ebe92ed53ac14b59e86b
Murilo <murilopizolito@gmail.com>
2026-04-13 21:47:13 -0300

    Seção 4 - Acesso a dados Com Spring Data JPA (Cascade)

--- libraryapi/src/main/java/br/com/murilo/libraryapi/model/Livro.java
+++ libraryapi/src/main/java/br/com/murilo/libraryapi/model/Livro.java
@@ -36,7 +36,7 @@ public class Livro {
     private BigDecimal preco;
 
     // Many = se refere a entidade atual / to / One = refere ao mapeamento
-    @ManyToOne // muitos para um - Um autor pode ter 1 ou mais livros
+    @ManyToOne//(cascade = CascadeType.ALL) // muitos para um - Um autor pode ter 1 ou mais livros / usando cascade. Operação em cascata, qualquer alteração no livro ele vai trazer o autor junto (executar na tabela de autor tbm)
     @JoinColumn(name = "id_autor") // relacionamento da coluna
     private Autor autor;
 
--- libraryapi/src/test/java/br/com/murilo/libraryapi/repository/LivroRepositoryTest.java
+++ libraryapi/src/test/java/br/com/murilo/libraryapi/repository/LivroRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootTest
@@ -28,7 +29,7 @@ class LivroRepositoryTest {
         livro.setIsbn("98886-84746");
         livro.setPreco(BigDecimal.valueOf(100));
         livro.setGenero(GeneroLivro.FICCAO);
-        livro.setTitulo("UFO");
+        livro.setTitulo("Outro Livro");
         livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
 
         Autor autor = autorRepository
@@ -38,6 +39,74 @@ class LivroRepositoryTest {
         livro.setAutor(autor);
 
         livroRepository.save(livro);
+    }
+
+    // opção padrão de implementação
+    @Test
+    void salvarAutorELivroTest() {
+        Livro livro = new Livro();
+        livro.setIsbn("98886-84746");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Terceiro Livro");
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+
+        Autor autor = new Autor();
+        autor.setNome("José");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1952, 1, 31));
+
+        autorRepository.save(autor);
+
+        livro.setAutor(autor);
+
+        livroRepository.save(livro);
+    }
+
+    //salvando em cascata, não salvei o autor no banco pelo código, mas ele faz isso automaticamente antes de solvar o livro, por conta do Cascade
+    @Test
+    void salvarCascadeTest() {
+        Livro livro = new Livro();
+        livro.setIsbn("98886-84746");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Outro Livro");
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+
+        Autor autor = new Autor();
+        autor.setNome("João");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1952, 1, 31));
+
+        livro.setAutor(autor);
+
+        livroRepository.save(livro);
+    }
+
+    @Test
+    void atualizarAutorDoLivro() {
+        UUID id = UUID.fromString("60a294a7-4da7-4540-bc69-efcf19172daa");
+        var livroParaAtualizar = livroRepository.findById(id).orElse(null);
+
+        UUID idAutor = UUID.fromString("aeb8cf56-bf10-4fd2-a9ea-0c2a192877be");
+        Autor maria = autorRepository.findById(idAutor).orElse(null);
+
+        livroParaAtualizar.setAutor(maria);
+
+        livroRepository.save(livroParaAtualizar);
+    }
+
+    @Test
+    void deletar() {
+        UUID id = UUID.fromString("60a294a7-4da7-4540-bc69-efcf19172daa");
+        livroRepository.deleteById(id);
+    }
+
+    // Usar com cuidado, ele deleta o autor junto por conta da operação em cascata. Poderia acontecer desse autor ser dono de outros livros, isso geraria um problema
+    @Test
+    void deletarCascade() {
+        UUID id = UUID.fromString("20c8dee5-ebab-4cd6-8a47-ef1385d3d4e6");
+        livroRepository.deleteById(id);
     }
 
 
